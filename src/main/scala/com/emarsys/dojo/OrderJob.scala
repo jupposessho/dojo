99c1561d7e1e9fd6fa26832331b89b09819d0138
Feher Vilmos <jupposessho@gmail.com>
2016-09-27 23:04:04 +0200

    [orderjob] refactor

--- src/main/scala/com/emarsys/dojo/OrderJob.scala
+++ src/main/scala/com/emarsys/dojo/OrderJob.scala
@@ -2,28 +2,31 @@ package com.emarsys.dojo
 
 object OrderJob {
 
-  def order(input: String): String = {
-    val jobDependencies = parse(input)
+  def order(input: String): String = pipeline(input)
 
-    if (jobDependencies.forall(_.noSelfDependency)) resolveDependency(jobDependencies).map(_.job).mkString("")
+  val pipeline = parse _ andThen checkSelfDependency andThen resolveDependency andThen toString
+
+  def parse(input: String) =
+    (clear(input) split '|').toList filterNot (_.isEmpty) map (f => JobDependency(f.head, f.tail.lastOption))
+
+  def checkSelfDependency(jobs: List[JobDependency]): List[JobDependency] =
+    if (jobs.forall(_.noSelfDependency)) jobs
     else throw new Exception("self dependency")
-  }
 
   def resolveDependency(jobs: List[JobDependency]): List[JobDependency] = jobs.partition(_.dependency.isEmpty) match {
     case (noDependency, Nil)            => noDependency
     case (Nil, withDependency)          => throw new Exception("circular dependency")
     case (noDependency, withDependency) => noDependency ++ resolveDependency(withDependency map {
-      w => removeResolvedDependency(w, noDependency.map(_.job))
+      w => removeResolvedDependency(w, noDependency map (_.job))
     })
   }
 
-  def removeResolvedDependency(jd: JobDependency, resolved: List[Char]) =
-    JobDependency(jd.job, jd.dependency.filterNot(resolved.contains(_)))
+  def toString(jobs: List[JobDependency]) = jobs.map(_.job).mkString("")
 
-  def parse(input: String): List[JobDependency] = {
-    if (input.length < 1) List.empty[JobDependency]
-    else input.split('|').toList.map(f => JobDependency(f.head, if (f.last == '>') None else f.lastOption))
-  }
+  val removeResolvedDependency = (jd: JobDependency, resolved: List[Char]) =>
+    JobDependency(jd.job, jd.dependency filterNot (resolved contains _))
+
+  val clear = (s: String) => s filter (c => '|' :: ('a' to 'z').toList contains c)
 
   case class JobDependency(job: Char, dependency: Option[Char]) {
     val noSelfDependency = dependency.fold(true)(_ != job)
