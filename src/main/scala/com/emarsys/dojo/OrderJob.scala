10ecaf7c4fb7e50a0d7816ae82d69d67b2ce5002
waff <vFeher@emarsys.com>
2016-09-29 10:46:43 +0200

    [orderjob] use only pure functions

--- src/main/scala/com/emarsys/dojo/OrderJob.scala
+++ src/main/scala/com/emarsys/dojo/OrderJob.scala
@@ -1,32 +1,35 @@
 package com.emarsys.dojo
 
-object OrderJob {
+trait OrderJob {
 
-  def order(input: String): String = pipeline(input)
+  import OrderJob._
 
-  val pipeline: (String) => String = parse _ andThen checkSelfDependency andThen resolveDependency andThen extractIds andThen toString
+  val ordered: (String) => String = parse _ andThen resolveDependency
 
-  def parse(input: String): List[Job] =
-    (clear(input) split '|').toList filterNot (_.isEmpty) map (f => Job(f.head, f.tail.lastOption))
+  def parse(input: String): List[Job] = {
+    val validChars = '|' :: ('a' to 'z').toList
+    val cleared    = input filter (c => validChars contains c)
 
-  def checkSelfDependency(jobs: List[Job]): List[Job] =
-    jobs.find(_.hasSelfDependency).fold(jobs)(_ => throw new Exception("self dependency"))
-
-  def resolveDependency(jobs: List[Job]): List[Job] = jobs.partition(_.dependency.isEmpty) match {
-    case (noDependency, Nil)            => noDependency
-    case (Nil, withDependency)          => throw new Exception("circular dependency")
-    case (noDependency, withDependency) => noDependency ++ resolveDependency(withDependency map { w =>
-      removeResolvedDependency(w, extractIds(noDependency))
-    })
+    (cleared split '|').toList filterNot (_.isEmpty) map (f => Job(f.head, f.tail.lastOption))
   }
 
-  def extractIds(jobs: List[Job]) = jobs map (_.id)
-  def toString(ids: List[Char])   = ids mkString ""
-  val validCharacters             = '|' :: ('a' to 'z').toList
-  val clear                       = (s: String) => s filter (c => validCharacters contains c )
-  val removeResolvedDependency    = (jd: Job, resolved: List[Char]) =>
-    Job(jd.id, jd.dependency filterNot (resolved contains _))
+  def resolveDependency(jobs: List[Job]): String = {
 
+    def acc(jobs: List[Job], result: String): String = jobs.partition(_.dependency.isEmpty) match {
+      case (resolved, Nil)        => result + (resolved map (_.id) mkString)
+      case (Nil, unresolved)      => "circular dependency"
+      case (resolved, unresolved) =>
+        val resolvedIds = resolved map (_.id)
+        acc(unresolved map (unResolveDependency(_, resolvedIds)), result + resolvedIds.mkString)
+    }
+
+    jobs.find(_.hasSelfDependency).fold(acc(jobs, ""))(_ => "self dependency")
+  }
+
+  def unResolveDependency(jd: Job, resolved: List[Char]) = Job(jd.id, jd.dependency filterNot (resolved contains _))
+}
+
+object OrderJob extends OrderJob {
   case class Job(id: Char, dependency: Option[Char]) {
     val hasSelfDependency = dependency.fold(false)(_ == id)
   }
--- src/test/scala/com/emarsys/dojo/OrderJobSpec.scala
+++ src/test/scala/com/emarsys/dojo/OrderJobSpec.scala
@@ -8,39 +8,31 @@ class OrderJobSpec extends WordSpec with Matchers {
   "OrderJob" should {
 
     "return empty in case of empty input" in {
-      order("") shouldEqual ""
+      ordered("") shouldEqual ""
     }
 
     "return single job in case of one element in input" in {
-      order("a =>") shouldEqual "a"
+      ordered("a =>") shouldEqual "a"
     }
 
     "return jobs in case of sequence of jobs without dependency in input" in {
-      order("a =>|b =>|c =>") shouldEqual "abc"
+      ordered("a =>|b =>|c =>") shouldEqual "abc"
     }
 
     "return jobs in case of multiple jobs single dependency in input" in {
-      order("a =>|b => c|c =>") shouldEqual "acb"
+      ordered("a =>|b => c|c =>") shouldEqual "acb"
     }
 
     "return jobs in case of multiple jobs multiple dependency in input" in {
-      order("a =>|b => c|c => f|d => a|e => b|f =>") shouldEqual "afcdbe"
+      ordered("a =>|b => c|c => f|d => a|e => b|f =>") shouldEqual "afcdbe"
     }
 
     "return jobs in case of multiple jobs self referencing dependency in input" in {
-      val caught = intercept[Exception] {
-          order("a =>|b =>|c => c")
-        }
-
-      caught.getMessage shouldEqual "self dependency"
+      ordered("a =>|b =>|c => c") shouldEqual "self dependency"
     }
 
     "return jobs in case of multiple jobs dependency chain in input" in {
-      val caught = intercept[Exception] {
-          order("a => b|b => c|c => a")
-        }
-
-      caught.getMessage shouldEqual "circular dependency"
+      ordered("a => b|b => c|c => a") shouldEqual "circular dependency"
     }
   }
 }
